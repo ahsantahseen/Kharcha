c16d3bfebbeef6bb7d1f3853937952068dc49582
Ahsan <58149036+ahsantahseen@users.noreply.github.com>
2022-05-13 02:56:36 +0500

    Front-end (Beta) Completed

--- app/src/main/java/com/example/kharcha/AddExpense.java
+++ app/src/main/java/com/example/kharcha/AddExpense.java
@@ -2,7 +2,9 @@ package com.example.kharcha;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddExpense extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class AddExpense extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_expense);
+
+    }
+    public void goToCalculator(View view){
+        Intent startActivity = new Intent(this, Intent.CATEGORY_APP_CALCULATOR.getClass());
+        startActivity(startActivity);
     }
 }
